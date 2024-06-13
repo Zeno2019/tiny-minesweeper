6a847b80f5c93a1b4856ac452b29cbc63fa626f3
Zeno_ <lin1071833138@gmail.com>
2024-06-13 22:35:47 +0800

    Fixed: style

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header';
 import { cn } from '@/lib/utils';
 
 export default function BaseLayout({ title, children, className }: { title: string; children?: React.ReactNode; className?: string }) {
-  const cls = cn('container', 'h-screen', 'bg-gray-100', className);
+  const cls = cn('container', 'min-h-screen', 'bg-gray-100', className);
 
   return (
     <div className={cls}>
