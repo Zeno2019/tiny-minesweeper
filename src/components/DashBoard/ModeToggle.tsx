5896d3ab387135178a0e4d4acd9067347aef7ddf
Zeno_ <lin1071833138@gmail.com>
2024-06-19 01:21:09 +0800

    Feat: 升级 Dev Mode 样式

--- src/components/DashBoard/ModeToggle.tsx
+++ src/components/DashBoard/ModeToggle.tsx
@@ -1,12 +1,17 @@
-import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
 import { MineSweeper } from '@/logic';
+import { useSnapshot } from 'valtio';
 
 export default function ModeToggle() {
+  const { devMode } = useSnapshot(MineSweeper.state);
+
   return (
-    <div className='grid place-items-center'>
-      <Button variant='ghost' className='text-lg bg-indigo-600 text-slate-100 hover:bg-indigo-700 hover:text-slate-100' onClick={() => MineSweeper.toggleDevMode()}>
-        DEV MODE
-      </Button>
+    <div className='flex items-center space-x-2'>
+      <Label htmlFor='dev-mode-switch' className='text-lg'>
+        Dev Mode
+      </Label>
+      <Switch id='dev-mode-switch' className='data-[state=checked]:bg-indigo-600' checked={devMode} onCheckedChange={() => MineSweeper.toggleDevMode()} />
     </div>
   );
 }
